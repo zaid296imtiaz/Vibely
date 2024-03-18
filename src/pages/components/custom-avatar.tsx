6e7b328baab23e5ba23bfff8755f3340f0b9169b
Zaid Imtiaz <zaid296imtiaz@gmail.com>
2024-03-17 21:06:31 -0400

    Fixed and updated the avatar

--- src/pages/components/custom-avatar.tsx
+++ src/pages/components/custom-avatar.tsx
@@ -1,9 +1,24 @@
-import {Avatar as AntdAvatar} from 'antd'
+import { Avatar as AntdAvatar, AvatarProps } from "antd";
 
-export const CustomAvatar = () => {
-  return (
-    <AntdAvatar>
-        ZD
-    </AntdAvatar>
-  )
+type Props = AvatarProps & {
+    name?: string
 }
+
+export const CustomAvatar = ({name, style,...rest}: Props) => {
+  return (
+    <AntdAvatar
+      alt={name}
+      size="small"
+      style={{
+        backgroundColor: "#68add0",
+        display: "flex",
+        alignItems: "center",
+        border: "none",
+        ...style
+      }}
+      {...rest}
+    >
+      {name}
+    </AntdAvatar>
+  );
+};
--- src/pages/components/layout/current-user.tsx
+++ src/pages/components/layout/current-user.tsx
@@ -1,8 +1,13 @@
 import { Popover, Button } from "antd";
 import React from "react";
 import { CustomAvatar } from "../custom-avatar";
+import { useGetIdentity } from "@refinedev/core";
+
+import type {User} from '@/graphql/schema.types'
 
 const CurrentUser = () => {
+  const { data: user } = useGetIdentity<User>();
+
   return (
     <>
       <Popover
@@ -11,7 +16,12 @@ const CurrentUser = () => {
         overlayInnerStyle={{ padding: 0 }}
         overlayStyle={{ zIndex: 999 }}
       >
-        <CustomAvatar/>
+        <CustomAvatar 
+        name={user?.name}
+        src={user?.avatarUrl}
+        size="default"
+        style={{cursor: 'pointer'}}
+        />
       </Popover>
     </>
   );

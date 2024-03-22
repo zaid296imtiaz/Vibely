3f2e376418ace731a52c0f49d0df43ec39800efb
Zaid Imtiaz <zaid296imtiaz@gmail.com>
2024-03-22 10:05:40 -0400

    Update resources.tsx

--- src/config/resources.tsx
+++ src/config/resources.tsx
@@ -1,24 +1,49 @@
-import { DashboardOutlined } from "@ant-design/icons";
+import {
+  DashboardOutlined,
+  ProjectOutlined,
+  ShopOutlined,
+} from "@ant-design/icons";
 import { IResourceItem } from "@refinedev/core";
 
 export const resources: IResourceItem[] = [
-    /** 
-     * WIKI:
-     * 
-     * list -> get all records (Read)
-     * show -> get single record (Read)
-     * create -> create a record (Create)
-     * edit -> update a record (Update)
-     * delete -> delete a record (Delete)
-     * clone -> copy a record
-     */
+  /**
+   * WIKI:
+   *
+   * list -> get all records (Read)
+   * show -> get single record (Read)
+   * create -> create a record (Create)
+   * edit -> update a record (Update)
+   * delete -> delete a record (Delete)
+   * clone -> copy a record
+   */
 
-    {
-        name: 'dashboard',
-        list: '/',
-        meta: {
-            label: 'Dashboard',
-            icon: <DashboardOutlined/>
-        }
-    }
-]
+  {
+    name: "dashboard",
+    list: "/",
+    meta: {
+      label: "Dashboard",
+      icon: <DashboardOutlined />,
+    },
+  },
+  {
+    name: "companies",
+    list: "/",
+    show: "/companies/:id",
+    create: "companies/new",
+    edit: "companies/edit/:id",
+    meta: {
+      label: "Companies",
+      icon: <ShopOutlined />,
+    },
+  },
+  {
+    name: "tasks",
+    list: "/tasks",
+    create: "tasks/new",
+    edit: "tasks/edit/:id",
+    meta: {
+      label: "Tasks",
+      icon: <ProjectOutlined />,
+    },
+  },
+];

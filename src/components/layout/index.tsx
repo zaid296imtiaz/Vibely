a2abfd11bb8a37cdc9bcd9761468d8dc3b727a60
Zaid Imtiaz <zaid296imtiaz@gmail.com>
2024-04-02 10:01:19 -0400

    Minor updates

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,11 +1,12 @@
 import { ThemedLayoutV2, ThemedTitleV2 } from "@refinedev/antd";
 import { Header } from "./header";
+import { DashboardOutlined, FireFilled } from "@ant-design/icons";
 
 export const Layout = ({ children }: React.PropsWithChildren) => {
   return (
     <ThemedLayoutV2
       Header={Header}
-      Title={(titleProps) => <ThemedTitleV2 {...titleProps} text="Refine" />}
+      Title={(titleProps) => <ThemedTitleV2 {...titleProps} text="Vibely" icon={<FireFilled />} />}
     >
       {children}
     </ThemedLayoutV2>
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,14 @@
-import { AuthPage } from "@refinedev/antd";
+import { AuthPage, ThemedTitleV2 } from "@refinedev/antd";
 import { authCredentials } from "../../providers";
+import { DashboardOutlined, FireFilled } from "@ant-design/icons";
 
 export const Login = () => {
   return (
     <AuthPage
       type="login"
+      registerLink={false}
+      forgotPasswordLink={false}
+      title={<ThemedTitleV2 icon={<FireFilled />} collapsed={false} text="Vibely CRM" />}
       formProps={{
         initialValues: authCredentials,
       }}
